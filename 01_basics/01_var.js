5df6d7095c07f3da5ba7e32bfc43d50d6a44bc03
Your Name <97039791+SCR01@users.noreply.github.com>
2025-07-20 11:52:06 +0530

    JS- Revision

---
+++ 01_basics/01_var.js
@@ -0,0 +1,11 @@
+const Acc = 22222;
+let accountEmail = "krish@gmail.com";
+var b = "2303";
+accountCity = "Delhi";
+
+
+accountEmail ="sharadreddy11@gmail.com";
+accountCity = "Bangalore";
+b = "2304";
+console.log([accountEmail, accountCity, b]);
+
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,16 @@
+/*let score = 22;
+console.log(typeof score); // number
+score = undefined;
+console.log(typeof score); // string
+let valueInNum = Number(score);
+console.log(typeof valueInNum); // number
+console.log(valueInNum); // 22 */
+
+let bulli = "";
+let bulla = Boolean(bulli);
+console.log(bulla); // true
+
+let somNum = 22;
+let someStr = String(somNum);
+console.log(someStr); // "22"
+console.log(typeof someStr); // string
