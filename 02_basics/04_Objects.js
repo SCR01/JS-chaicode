cc83731931fb487665fb151224851f7276ff7d32
Your Name <97039791+SCR01@users.noreply.github.com>
2025-07-25 07:36:06 +0530

    lesson-2

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,48 @@
+//const tinderUser = new Object() singelton object
+
+
+const tinderUser = {} // non-singleton
+
+tinderUser.id = "123"
+tinderUser.name = "babbu"
+tinderUser.isLoggedIn = false
+
+/* const regular ={
+    name: "baba",
+    game:{
+        
+       userfull:{
+        firstname: "rahul",
+        lastname: "reddy"
+       }
+    }
+} */
+ 
+//console.log(regular.game)
+
+/* const onj1 = {
+    1:"a",2:"b"
+}
+const onj2 = {
+    3:"a",4:"2"
+}
+
+//const obj3 = Object.assign({}, onj1, onj2)//better syntax
+const obj3 = {...onj1,...onj2}//easy syntax
+console.log(obj3) */
+
+const users = [{
+        id: 1,
+        email: "hello123@gmail.com"
+    },
+    {
+        id:2,
+        lol:"lksdjf@gamil.com"
+    }
+]
+    
+
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
